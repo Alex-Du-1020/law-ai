a98ae2afe5159d81098c02ab93a145d98e0de51b
Alex_Du <hust.duxin@gmail.com>
2025-07-04 11:53:56 +0800

    Import vector db.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-. bin/activate
-python3 hello.py
+
+python3 -m venv my-venv
+
+my-venv/bin/pip install chromadb sentence-transformers langchain python-dotenv requests
+
+source my-venv/bin/activate
+
+uvicorn src.query:app --reload
