b412039282c568b7d109d66e2e581c1a1afafa8b
Alex_Du <hust.duxin@gmail.com>
2025-07-04 12:13:37 +0800

    upgrade packages dependencies.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 python3 -m venv my-venv
 
-my-venv/bin/pip install chromadb sentence-transformers langchain python-dotenv requests
+my-venv/bin/pip install chromadb sentence-transformers langchain python-dotenv requests fastapi uvicorn
 
 source my-venv/bin/activate
 
